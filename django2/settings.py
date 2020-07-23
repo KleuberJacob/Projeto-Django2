ffc9446e834cb700d864ac5b47ee0bfaa87aa2cc
Kleuber Jacob <kleuber.18@hotmail.com>
2020-07-23 15:16:43 -0300

    Alteraçao na MEDIA_URL

--- django2/settings.py
+++ django2/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configurando sistema de envio de emails de teste dentro do próprio Django
